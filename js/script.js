4d07677b04121737770678424084d33055cc48df
kalister5 <zainkalister@gmail.com>
2018-11-30 14:31:29 +0300

    add more information about script.js

---
+++ js/script.js
@@ -0,0 +1,29 @@
+function triangle() {
+  var sides = [];
+  sides.push(document.getElementById("side1").value);
+  sides.push(document.getElementById("side2").value);
+  sides.push(document.getElementById("side3").value);
+
+  var [a, b, c] = sides;
+  if (a.length === 0 || b.length === 0 || c.length === 0) {} else {
+    side(a, b, c);
+  }
+}
+
+function side(a, b, c) {
+  if (a + b > c && b + c > a && c + a > b) {
+    if (a === b && a === c) {
+      display.innerHTML = "<p>equilateral triangle</p>"
+    } else if (a === b || b === c || a === c) {
+      display.innerHTML = "<p>isosceles</p>"
+    } else {
+      display.innerHTML = "<p>scalene</p>"
+    }
+  } else {
+    display.innerHTML = "<p>not a triangle</p>"
+  }
+}
+
+function reset() {
+  location.reload();
+}
